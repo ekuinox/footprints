b49e1e51cc6029029b60e99f42a16e094f2546f9
ekuinox <depkey@me.com>
2021-11-02 21:26:20 +0900

    zip対象が間違っていたのを修正

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ROOT=$(pwd)
 cd ./sample-lambda-func
 cross build --release --target x86_64-unknown-linux-musl && \
     mkdir -p ./target/cdk/release && \
-    zip -j ./target/cdk/release/bootstrap.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
+    zip -j ./target/cdk/release/bootstrap.zip ./target/x86_64-unknown-linux-musl/release/sample-lambda-func
 
 cd ${ROOT}
