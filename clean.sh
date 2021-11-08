850c4aaa1619d5c28c28491a19d9bf760f79f4ec
ekuinox <depkey@me.com>
2021-11-08 23:02:33 +0900

    build.sh に合わせて

--- clean.sh
+++ clean.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-ROOT=$(pwd)
+LAMBDA_FUNCTION_NAMES_FILE="./lambda_functions"
+FRONTEND_DIRECTORY="footprints-web"
 
-cd ./sample-lambda-func
+function clean_lambda() {
+    target=$1
+    prev_directory=`pwd`
 
-rm -r ./target/cdk/release || echo '[build:clean] No existing release found.'
+    echo "target=\"${target}\""
+    cd ${target}
+    rm -r ./target/cdk/release
+    cd ${prev_directory}
+}
 
-cd ${ROOT}
+function clean_frontend() {
+    target=$1
+    prev_directory=`pwd`
+
+    echo "target=\"${target}\""
+    # todo ...
+}
+
+while read name; do
+    test -e ${name} && \
+        clean_lambda ${name} || \
+        echo "target=${name} not found."
+done < ${LAMBDA_FUNCTION_NAMES_FILE}
+
+# todo .env
+clean_frontend ${FRONTEND_DIRECTORY}
