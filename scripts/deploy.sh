a362165b43f2b39ffbef592e11b9ea5e5bf18129
ekuinox <depkey@me.com>
2021-11-09 22:18:20 +0900

    add AWS_REGION

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,6 @@ source .env
 
 export COGNITO_PROVIDER_GOOGLE_CLIENT_ID=${COGNITO_PROVIDER_GOOGLE_CLIENT_ID}
 export COGNITO_PROVIDER_GOOGLE_CLIENT_SECRET=${COGNITO_PROVIDER_GOOGLE_CLIENT_SECRET}
+export AWS_REGION=${AWS_REGION}
 
 [[ $CI == 'true' ]] && export CDK_APPROVAL='never' || export CDK_APPROVAL='broadening'; cdk deploy --require-approval $CDK_APPROVAL '*'
